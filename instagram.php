9486fd7240657b8d225dd39bf3e1425ef6f2a1f5
Özgürcan <102454358+EironeMarian@users.noreply.github.com>
2024-02-27 18:17:50 +0300

    Update instagram.php

--- instagram.php
+++ instagram.php
@@ -10,13 +10,11 @@ if (!ctype_alnum($username)) {
 $url = 'https://pixwox.net/profile/' . $username;
 $html = file_get_contents($url);
 
-// Veri var mı kontrol et
 if (!$html) {
     echo json_encode(["error" => "Veri alınamadı"]);
     exit;
 }
 
-// Düzenli ifade kullanarak postCount, followersCount ve followingsCount verilerini bul
 $postCount = $followersCount = $followingsCount = 0;
 preg_match('/var postCount = (\d+);/', $html, $postMatches);
 preg_match('/var followersCount = (\d+);/', $html, $followersMatches);
@@ -28,69 +26,51 @@ if (empty($postMatches) || empty($followersMatches) || empty($followingsMatches)
     exit;
 }
 
-// Eşleşen değerleri al
 $postCount = $postMatches[1];
 $followersCount = $followersMatches[1];
 $followingsCount = $followingsMatches[1];
 
-// HTML içeriğini DOMDocument'e yükle
 $dom = new DOMDocument();
-@$dom->loadHTML($html); // Uyarıları bastırmak için @ kullanıldı, pratikte daha iyi hata yönetimi sağlamak için kaldırılabilir
+@$dom->loadHTML($html); 
 
-// Belirli sınıf içindeki img etiketlerini seç
 $xpath = new DOMXPath($dom);
-$class = "col-lg-4 text-center"; // İşlem yapmak istediğiniz sınıf
+$class = "col-lg-4 text-center";
 $query = "//div[contains(concat(' ', normalize-space(@class), ' '), ' $class ')]//img";
 $images = $xpath->query($query);
 
-// Belirli sınıf içindeki text içeriklerini seç
-$class = "card-title text-primary fw-medium"; // İşlem yapmak istediğiniz sınıf
+$class = "card-title text-primary fw-medium";
 $query = "//h5[contains(concat(' ', normalize-space(@class), ' '), ' $class ')]";
 $texts = $xpath->query($query);
 
-// Kontrol edilecek metin
 $checkText = "bi bi-patch-check-fill text-primary";
 
-// HTML içeriğinde kontrol metnini ara
 $htmlContainsCheckText = strpos($html, $checkText) !== false;
 
-// JSON verisi için bir dizi oluştur
 $jsonData = array();
 
-// Her bir img etiketi için işlem yap
 foreach ($images as $image) {
-    // img etiketinin src özniteliğini al
     $src = $image->getAttribute('src');
     
-    // Eğer src özniteliği belirtilen önek içeriyorsa
     if (strpos($src, 'https://cdn2.pixwox.net/index.php/') !== false) {
-        // Öneki kaldır ve geri kalan kısmı $newSrc değişkenine ata
         $newSrc = str_replace('https://cdn2.pixwox.net/index.php/', '', $src);
         
-        // JSON verisine ekle
         $jsonData["profil_photo"] = $newSrc;
     }
 }
 
-// Her bir text içeriği için işlem yap
 foreach ($texts as $text) {
-    // text içeriğini al
     $name = $text->nodeValue;
 
-    // JSON verisine ekle
     $jsonData["name"] = $name;
 }
 
-// blue_tick değerini belirle
 $blueTick = $htmlContainsCheckText ? true : false;
 $jsonData["blue_tick"] = $blueTick;
 
-// JSON verisini yazdır
 $jsonData["postCount"] = $postCount;
 $jsonData["followersCount"] = $followersCount;
 $jsonData["followingsCount"] = $followingsCount;
 
-// JSON verisini yazdırırken hata ayıklama yap
 $json = json_encode($jsonData, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 
     $jsonData = str_replace('\/', '/', $json);
